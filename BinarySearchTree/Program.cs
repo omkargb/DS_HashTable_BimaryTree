af0b6d99edcb064964a59e36c004fc360017e82d
omkargb <omkargb107@gmail.com>
2021-08-15 22:00:22 +0530

    [omkargb] Add : program files aded and Welcome message displayed

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("  Welcome to Binary search tree program...");
+
+        }
+    }
+}
